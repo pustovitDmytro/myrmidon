a016a81148285a0419eedad93045ff285effae7d
dmytroPust <dipustovit@gmail.com>
2020-05-23 15:56:28 +0300

    Docs: adds documentation for custom helpers

--- src/custom.js
+++ src/custom.js
@@ -8,7 +8,6 @@ function abort(error = new Error('Unknown Retry Abortion')) {
     throw error;
 }
 
-// timeout : /number/{min, max, factor, randomize}/function(retryAttempt) Exponential Backoff
 function calculateTimeout(opts, attempt = 0) {
     if (isNumber(opts)) return opts;
     if (isFunction(opts)) return opts(attempt);
@@ -19,6 +18,15 @@ function calculateTimeout(opts, attempt = 0) {
     }
 }
 
+/**
+ * Retrying function calls on errors
+ * @param {function} retrier function (sync or async) that will be retried
+ * @param {object} settings method configuration
+ * @param {function} [settings.onRetry] function that will be called on every retry attempt. Receives (error, iter, abortedKey). If error is thrown from onRetry, retrying is aborted.
+ * @param {number} [settings.retries=10] max retry times
+ * @param {(number|object|function)} [settings.timeout=100] timeout configuration. If number specifies time in ms, if object - specifies Exponential Backoff with properties min, max, factor, randomize
+ * @returns {any} result of retrier function call
+ */
 export function retry(retrier, { onRetry, retries = 10, timeout = 100 } = {}) {
     function onError(err, iter, rej) {
         if (onRetry) onRetry(err, iter, abortedKey);
@@ -43,6 +51,14 @@ export function retry(retrier, { onRetry, retries = 10, timeout = 100 } = {}) {
     });
 }
 
+/**
+ * Get nested property of object
+ * @param {object} obj input data
+ * @param {string} path property path
+ * @param {object} settings method configuration
+ * @param {string} [settings.delimeter='.'] path delimeter notation
+ * @returns {any} deep object property, or null if nothing found
+ */
 export function getProp(obj, path, { delimeter = '.' } = {}) {
     return path.split(delimeter).reduce((accum, pathElem) => {
         try {
@@ -53,6 +69,15 @@ export function getProp(obj, path, { delimeter = '.' } = {}) {
     }, obj);
 }
 
+/**
+ * Fills string template with specified data
+ * @param {string} template string template
+ * @param {object} data input data
+ * @param {object} settings method configuration
+ * @param {array} [settings.delimiters=[ '{', '}' ]] literal delimiter flags
+ * @param {regExp} [settings.regExp] regexp to parse template (replaces full match with groups)
+ * @returns {string} filled template
+ */
 export function fill(template, data, { delimiters = [ '{', '}' ], regExp } = {}) {
     const regexp = regExp || new RegExp(`${delimiters[0]}(.+?)${delimiters[1]}`, 'g');
 
