b4328d6ea1ead1a25df99e1f431f9dd5061eff52
dmytroPust <dipustovit@gmail.com>
2020-06-18 17:20:58 +0300

    Chore: adds cleanUndefined test

---
+++ tests/helpers/object/cleanUndefined.test.js
@@ -0,0 +1,15 @@
+import { cleanUndefined } from '../../entry';
+import { FunctionTester } from '../../utils';
+
+const tester = new FunctionTester(cleanUndefined);
+
+suite('CheckTypes: cleanUndefined');
+
+test('Positive: cleanUndefined @example', () => {
+    tester.test({ x: { a: null, b: undefined }, c: 0 }, { x: { a: null }, c: 0 });
+});
+
+after(async () => {
+    // console.log('after', this);
+});
+
