948da5ee1bef93e4e80533d35ac896d72d108e28
Dmytro Pustovit <dipustovit@gmail.com>
2021-02-12 22:58:58 +0200

    Fix: cleanUndefined on circullar structure

--- tests/helpers/object/cleanUndefined.test.js
+++ tests/helpers/object/cleanUndefined.test.js
@@ -1,3 +1,4 @@
+import { assert } from 'chai';
 import { cleanUndefined } from '../../entry';
 import { FunctionTester } from '../../utils';
 
@@ -7,6 +8,13 @@ suite('object: cleanUndefined');
 
 test('Positive: cleanUndefined @example', () => {
     tester.test({ x: { a: null, b: undefined }, c: 0 }, { x: { a: null }, c: 0 });
+});
+
+test('Positive: cleanUndefined on circullar structure', () => {
+    const obj = { a: { b: null, c: undefined } };
+
+    obj.a.obj = obj;
+    assert.deepEqual(cleanUndefined(obj), { a: { b: null, obj } });
 });
 
 after(async () => {
