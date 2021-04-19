b28710a7b6e5f333ebe99d6eabde7463023ac92d
Dmytro Pustovit <dipustovit@gmail.com>
2021-04-20 00:15:45 +0300

    Chore: fixes semantic-release command on deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,4 @@ BIN_DIR=$(dirname "${BASH_SOURCE[0]}")
 npm run save-examples
 npx babel-node $BIN_DIR/documentation.js readme
 npx babel-node $BIN_DIR/documentation.js reference
-npm run push:release
+npm run semantic-release
